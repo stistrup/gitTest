5003309ee854d1d8507976b595edc4432f76bc55
stistrup <31688797+stistrup@users.noreply.github.com>
2018-03-08 13:09:48 +0100

    wow

---
+++ pappa.py
@@ -0,0 +1,12 @@
+
+självförtroende = False
+
+def tjenare_Brorsan():
+    saker_man_kan_säga = "hejsan"
+    saker_man_också_kan_säga = "eeh.. förlåt.. för allt.."
+
+    if självförtroende:
+        return saker_man_kan_säga
+    else:
+        return saker_man_också_kan_säga
+    
